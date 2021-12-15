f34e1cb4519a1337a43826840e41e7c8580ea921
Limsa00 <bennis.salim@gmail.com>
2021-12-15 10:47:03 +0100

    change client baseURL & rename index.js to server.js

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,4 +1,5 @@
-export const baseURL = 'http://localhost:3000/v1';
+export const baseURL = 'https://young-dusk-15793.herokuapp.com/v1';
+// export const baseURL = 'http://localhost:3000/v1';
 // export const baseURL = 'https://globetrotter-973.herokuapp.com/v1';
 export const appId = 'plP2U9KPBVXF'; // Token Algolia Application id
 export const apiKey = '031d6d4d8488a26b60b59b7f78c474dc'; // Token Algolia API key
